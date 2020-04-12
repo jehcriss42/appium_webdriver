45b1bc938793736b177af7653ee0b2d1f1737aef
Jessica Tavares <jehcriss42@gmail.com>
2020-04-12 02:24:35 -0300

    added test examples for practice

--- test/specs/sample.test.js
+++ test/specs/sample.test.js
@@ -1,5 +1,26 @@
 describe('sample',()=>{
-    it('Test',()=>{
-
+    it('Test different elements',()=>{
+        // AccessibilityID
+        $('~App').click();
+        $('~Alert Dialogs').click();
+        $('~Text Entry dialog').click();
+        // Custom XPATH
+        $('//android.widget.EditText[@resource-id="io.appium.android.apis:id/username_edit"]').addValue("Test User");
+        $('//android.widget.EditText[@resource-id="io.appium.android.apis:id/password_edit"]').addValue("password")
+        $('//android.widget.Button[@resource-id="android:id/button1"]').click();
+        driver.back();
+        driver.back();
+        driver.back();
     })
+
+    it('Test scroll',()=>{
+        // touchAction
+        driver.touchAction([
+            { action: 'press', x:500, y: 1000 },
+            { action: 'moveTo', x: 500, y:700 },
+            'release'
+        ])
+        $('~Views').click();
+    })
+
 })
